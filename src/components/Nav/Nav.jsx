824d0bef3d6224118942b1322dc6f60bc7ac5467
JCMartinez <jcmtz.091@gmail.com>
2023-09-26 10:39:10 -0600

    feat: create head responsive and create nav logos component

---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import './Nav.css'
+
+const Nav = () => {
+    return (
+        <>
+            <div className="nav-container">
+                <div className="nav-clarifion-logo-container">
+                    <img src="../../../src/assets/icons/Clarifion_Logo_1.png" />
+                </div>
+                <div className="nav-anti-virus-logos-container">
+                    <img src="../../../src/assets/icons/McAffee.png" />
+                    <img src="../../../src/assets/icons/norton-antivirus-logo_1.png" />
+                </div>
+            </div>
+        </>
+    )
+
+}
+
+export default Nav
---
+++ src/views/Main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import USPS from "../components/USPS/USPS.jsx";
+import Nav from "../components/Nav/Nav.jsx";
+const Main = () => {
+    return (
+        <>
+            <USPS />
+            <Nav />
+        </>
+    )
+}
+
+export default Main
