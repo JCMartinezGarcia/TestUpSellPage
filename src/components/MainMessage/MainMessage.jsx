f9f01cadaa785d3281b1f922a8733cfd710a8c50
JCMartinez <jcmtz.091@gmail.com>
2023-09-26 11:18:27 -0600

    feat: create main message component

---
+++ src/components/MainMessage/MainMessage.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import './MainMessage.css';
+
+
+const MainMessage = () => {
+    return (
+        <>
+            <div className="main-message-container">
+                <p className="main-message">Wait! Your Order In Progress.</p>
+                <p className="main-sub-message">Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing</p>
+            </div>
+        </>
+    )
+}
+
+export default MainMessage;
