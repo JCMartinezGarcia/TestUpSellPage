a9c3e97c42fcd6d8e3d9d52551ce1210df9dae3e
JCMartinez <jcmtz.091@gmail.com>
2023-09-27 09:09:24 -0600

    feat: create steps component

--- src/views/Main.jsx
+++ src/views/Main.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import USPS from "../components/USPS/USPS.jsx";
 import Nav from "../components/Nav/Nav.jsx";
 import MainMessage from "../components/MainMessage/MainMessage.jsx";
+import Steps from "../components/Steps/Steps.jsx";
 const Main = () => {
     return (
         <>
             <USPS />
             <Nav />
             <MainMessage />
+            <Steps />
         </>
     )
 }
