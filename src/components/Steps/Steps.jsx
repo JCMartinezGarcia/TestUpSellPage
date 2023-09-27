04738c2ee22e015711e98526a6b6b1ea5e590e6c
JCMartinez <jcmtz.091@gmail.com>
2023-09-27 09:47:05 -0600

    feat: make steps component responsive, size 428

--- src/components/Steps/Steps.jsx
+++ src/components/Steps/Steps.jsx
@@ -6,10 +6,10 @@ const Steps = () => {
     return (
         <>
             <div className="steps-container">
-                <span className="steps-items"><img src="../../../src/assets/icons/checkmark.png" /><p>Step 1 :Cart Review</p></span>
-                <span className="steps-items"><img src="../../../src/assets/icons/checkmark.png" /><p>Step 2 :Checkout</p></span>
-                <span className="steps-items"><img src="../../../src/assets/icons/checkmark3.png" /><p><strong>Step 3 :Special Offer</strong></p></span>
-                <span className="steps-items"><img src="../../../src/assets/icons/checkmark4.png" /><p>Step 4 :Confirmation</p></span>
+                <span className="steps-items"><img src="../../../src/assets/icons/checkmark.png" /><p><span className="step-number-item">Step 1 :</span>Cart Review</p></span>
+                <span className="steps-items"><img src="../../../src/assets/icons/checkmark.png" /><p><span className="step-number-item">Step 2 :</span>Checkout</p></span>
+                <span className="steps-items"><img src="../../../src/assets/icons/checkmark3.png" /><p><strong><span className="step-number-item">Step 3 :</span>Special Offer</strong></p></span>
+                <span className="steps-items"><img src="../../../src/assets/icons/checkmark4.png" /><p><span className="step-number-item">Step 4 :</span>Confirmation</p></span>
             </div>
         </>
     )
